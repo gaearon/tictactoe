373b80239b2b6245cbb3805537cf70fe801644f5
Dan Abramov <dan.abramov@gmail.com>
2013-05-20 20:11:55 +0400

    Use Visual Studio spacing policy

--- Program.cs
+++ Program.cs
@@ -4,88 +4,88 @@ using System.Linq;
 
 namespace TicTacToe
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			var game = new TicTacToe ();
-			bool player = true;
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            var game = new TicTacToe ();
+            bool player = true;
 
-			while (!game.Winner.HasValue) {
-				Console.WriteLine ("{0}, what will be your move?\n", FormatPlayer (player));
-				Console.WriteLine (FormatGame (game));
-				Console.Write ("\nType A1 to C3: ", FormatPlayer (player));
+            while (!game.Winner.HasValue) {
+                Console.WriteLine ("{0}, what will be your move?\n", FormatPlayer (player));
+                Console.WriteLine (FormatGame (game));
+                Console.Write ("\nType A1 to C3: ", FormatPlayer (player));
 
-				try {
-					var move = ParseMove (Console.ReadLine ().Trim ());
-					game.MakeMove (move.Item1, move.Item2, player);
-					player = !player;
-				} catch (Exception ex) {
-					Console.WriteLine ("I can't do that. {0}", ex.Message);
-					continue;
-				} finally {
-					Console.WriteLine ("\n\n");
-				}
-			}
+                try {
+                    var move = ParseMove (Console.ReadLine ().Trim ());
+                    game.MakeMove (move.Item1, move.Item2, player);
+                    player = !player;
+                } catch (Exception ex) {
+                    Console.WriteLine ("I can't do that. {0}", ex.Message);
+                    continue;
+                } finally {
+                    Console.WriteLine ("\n\n");
+                }
+            }
 
-			foreach (var msg in FormatWinnerMessage (game.Winner.Value))
-				Console.Write (msg);
-		}
+            foreach (var msg in FormatWinnerMessage (game.Winner.Value))
+                Console.Write (msg);
+        }
 
-		static string FormatPlayer (bool player)
-		{
-			return FormatCell (player);
-		}
+        static string FormatPlayer (bool player)
+        {
+            return FormatCell (player);
+        }
 
-		static string FormatCell (bool? cell)
-		{
-			return cell.HasValue
+        static string FormatCell (bool? cell)
+        {
+            return cell.HasValue
 				? (cell.Value ? "X" : "O")
 				: "_";
-		}
+        }
 
-		static string FormatRow (IEnumerable<bool?> row)
-		{
-			return string.Join ("|", row.Select (FormatCell));
-		}
+        static string FormatRow (IEnumerable<bool?> row)
+        {
+            return string.Join ("|", row.Select (FormatCell));
+        }
 
-		static string FormatGame (TicTacToe game)
-		{
-			return string.Join ("\n", game.GetRows ().Select (FormatRow));
-		}
+        static string FormatGame (TicTacToe game)
+        {
+            return string.Join ("\n", game.GetRows ().Select (FormatRow));
+        }
 
-		static Tuple<int, int> ParseMove (string input)
-		{
-			try {
-				var move = Tuple.Create (
-					input [0] - 'A',
-					input [1] - '1'
-				);
+        static Tuple<int, int> ParseMove (string input)
+        {
+            try {
+                var move = Tuple.Create (
+                    input [0] - 'A',
+                    input [1] - '1'
+                );
 
-				if (!ValidateMove (move))
-					throw new Exception ();
+                if (!ValidateMove (move))
+                    throw new Exception ();
 
-				return move;
+                return move;
 
-			} catch (Exception ex) {
-				throw new Exception ("Stick to simple things like A1 or C2.", ex);
-			}
-		}
+            } catch (Exception ex) {
+                throw new Exception ("Stick to simple things like A1 or C2.", ex);
+            }
+        }
 
-		static bool ValidateMove (Tuple<int, int> move)
-		{
-			return move.Item1 >= 0 && move.Item1 <= 2
-				&& move.Item2 >= 0 && move.Item2 <= 2;
-		}
+        static bool ValidateMove (Tuple<int, int> move)
+        {
+            return move.Item1 >= 0 && move.Item1 <= 2
+                && move.Item2 >= 0 && move.Item2 <= 2;
+        }
 
-		static IEnumerable<string> FormatWinnerMessage (bool player)
-		{
-			while (true) {
-				for (var i = 0; i < 30; i++)
-					yield return FormatPlayer (player);
+        static IEnumerable<string> FormatWinnerMessage (bool player)
+        {
+            while (true) {
+                for (var i = 0; i < 30; i++)
+                    yield return FormatPlayer (player);
 
-				yield return "   CONGRATULATIONS   ";
-			}
-		}
-	}
+                yield return "   CONGRATULATIONS   ";
+            }
+        }
+    }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -4,113 +4,114 @@ using System.Collections.Generic;
 
 namespace TicTacToe
 {
-	class TicTacToe {
-		const int Length = 3;
+    class TicTacToe
+    {
+        const int Length = 3;
 
-		private bool? [][] _data;
-		private bool? _winner;
+        private bool?[][] _data;
+        private bool? _winner;
 
-		public TicTacToe ()
-		{
-			_data = Enumerable
+        public TicTacToe ()
+        {
+            _data = Enumerable
 				.Range (0, Length)
 				.Select (_ => new bool? [Length])
 				.ToArray ();
-		}
+        }
 
-		public bool? GetCell (int row, int column)
-		{
-			return _data [row][column];
-		}
+        public bool? GetCell (int row, int column)
+        {
+            return _data [row] [column];
+        }
 
-		public IEnumerable<bool?> GetRow (int index)
-		{
-			return _data [index];
-		}
+        public IEnumerable<bool?> GetRow (int index)
+        {
+            return _data [index];
+        }
 
-		IEnumerable<int> GetIndices ()
-		{
-			return Enumerable.Range (0, Length);
-		}
+        IEnumerable<int> GetIndices ()
+        {
+            return Enumerable.Range (0, Length);
+        }
 
-		public IEnumerable<bool?> GetColumn (int index)
-		{
-			return GetIndices ()
+        public IEnumerable<bool?> GetColumn (int index)
+        {
+            return GetIndices ()
 				.Select (GetRow)
 				.Select (row => row.ElementAt (index));
-		}
+        }
 
-		public IEnumerable<bool?> GetDiagonal (bool ltr)
-		{
-			return GetIndices ()
+        public IEnumerable<bool?> GetDiagonal (bool ltr)
+        {
+            return GetIndices ()
 				.Select (i => Tuple.Create (i, ltr ? i : Length - 1 - i))
 				.Select (pos => GetCell (pos.Item1, pos.Item2));
-		}
+        }
 
-		public IEnumerable<IEnumerable<bool?>> GetRows ()
-		{
-			return GetIndices ()
+        public IEnumerable<IEnumerable<bool?>> GetRows ()
+        {
+            return GetIndices ()
 				.Select (GetRow);
-		}
+        }
 
-		public IEnumerable<IEnumerable<bool?>> GetColumns ()
-		{
-			return GetIndices ()
+        public IEnumerable<IEnumerable<bool?>> GetColumns ()
+        {
+            return GetIndices ()
 				.Select (GetColumn);
-		}
+        }
 
-		public IEnumerable<IEnumerable<bool?>> GetDiagonals ()
-		{
-			return new [] { true, false }
+        public IEnumerable<IEnumerable<bool?>> GetDiagonals ()
+        {
+            return new [] { true, false }
 				.Select (GetDiagonal);
-		}
+        }
 
-		public IEnumerable<IEnumerable<bool?>> GetVectors ()
-		{
-			return GetDiagonals ()
+        public IEnumerable<IEnumerable<bool?>> GetVectors ()
+        {
+            return GetDiagonals ()
 				.Concat (GetRows ())
 				.Concat (GetColumns ());
-		}
+        }
 
-		static bool? FindWinner (IEnumerable<bool?> vector)
-		{
-			try {
-				return vector
+        static bool? FindWinner (IEnumerable<bool?> vector)
+        {
+            try {
+                return vector
 					.Distinct ()
 					.Single ();
-			} catch (InvalidOperationException) {
-				return null;
-			}
-		}
+            } catch (InvalidOperationException) {
+                return null;
+            }
+        }
 
-		static bool? FindWinner (IEnumerable<IEnumerable<bool?>> vectors)
-		{
-			return vectors
+        static bool? FindWinner (IEnumerable<IEnumerable<bool?>> vectors)
+        {
+            return vectors
 				.Select (FindWinner)
 				.FirstOrDefault (v => v != null);
-		}
+        }
 
-		public bool? FindWinner ()
-		{
-			return FindWinner (GetVectors ());
-		}
+        public bool? FindWinner ()
+        {
+            return FindWinner (GetVectors ());
+        }
 
-		public bool MakeMove (int row, int column, bool move)
-		{
-			if (_winner.HasValue)
-				throw new InvalidOperationException ("The game is already won.");
+        public bool MakeMove (int row, int column, bool move)
+        {
+            if (_winner.HasValue)
+                throw new InvalidOperationException ("The game is already won.");
 
-			if (_data [row][column].HasValue)
-				throw new InvalidOperationException ("This cell is already taken.");
+            if (_data [row] [column].HasValue)
+                throw new InvalidOperationException ("This cell is already taken.");
 
-			_data [row][column] = move;
-			_winner = FindWinner ();
+            _data [row] [column] = move;
+            _winner = FindWinner ();
 
-			return move == _winner;
-		}
+            return move == _winner;
+        }
 
-		public bool? Winner {
-			get { return _winner; }
-		}
-	}
+        public bool? Winner {
+            get { return _winner; }
+        }
+    }
 }
