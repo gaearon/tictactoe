34aceaa23e3426805bb855bfad24fed6a58ea681
Dan Abramov <dan.abramov@gmail.com>
2014-01-05 23:07:39 +0400

    Actually, there's no need to check bounds here because access to _cells will throw anyway

--- Board.cs
+++ Board.cs
@@ -18,12 +18,8 @@ namespace TicTacToe
 
         public Player? this [int row, int column] {
             get {
-                CheckBounds (row, column);
-
                 return _cells [row] [column];
             } set {
-                CheckBounds (row, column);
-
                 if (this [row, column].HasValue)
                     throw new InvalidOperationException ("The cell is already claimed.");
 
@@ -37,15 +33,6 @@ namespace TicTacToe
                 .Range (0, size)
                 .Select (_ => new Player? [size])
                 .ToArray ();
-        }
-
-        void CheckBounds (int row, int column)
-        {
-            if (row < 0 || row >= Size)
-                throw new ArgumentOutOfRangeException ("row");
-
-            if (column < 0 || column >= Size)
-                throw new ArgumentOutOfRangeException ("column");
         }
     }
 }
